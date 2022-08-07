1045a3cf58cb238baf24d556ad1e1c991dd67943
Adrian1jr <adrian1jr@gmail.com>
2022-08-07 10:26:09 -0500

    Controller SEED tipandolo, CLEAN code y good code

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSeedDto } from './dto/create-seed.dto';
-import { UpdateSeedDto } from './dto/update-seed.dto';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  create(createSeedDto: CreateSeedDto) {
-    return 'This action adds a new seed';
-  }
+  private readonly axios: AxiosInstance = axios;
 
-  findAll() {
-    return `This action returns all seed`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} seed`;
-  }
-
-  update(id: number, updateSeedDto: UpdateSeedDto) {
-    return `This action updates a #${id} seed`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} seed`;
+  async executSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+    return data.results;
   }
 }
